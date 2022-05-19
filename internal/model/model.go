db76049a50b182460ad08a2cee195f55710ce4f5
axzed <1491224820@qq.com>
2022-05-19 15:53:15 +0800

    finish sql & create model

---
+++ internal/model/model.go
@@ -0,0 +1,12 @@
+package model
+
+// 公共的model
+type Model struct {
+	ID         uint32 `gorm:"primary_key" json:"id"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	CreatedOn  uint32 `json:"created_on"`
+	ModifiedOn uint32 `json:"modified_on"`
+	DeleteOn   uint32 `json:"delete_on"`
+	IsDel      uint8  `json:"is_del"`
+}
